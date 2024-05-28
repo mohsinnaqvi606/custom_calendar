0f77d060a24268659a0a9f80dfd4ff493c37d9f9
Mohsin Naqvi <Mohsinnaqvi606@gmail.com>
2024-05-28 09:16:57 +0500

    expand and collapse programmatically

--- lib/flutter_neat_and_clean_calendar.dart
+++ lib/flutter_neat_and_clean_calendar.dart
@@ -226,6 +226,7 @@ class _CalendarState extends State<Calendar> {
     if (widget.scrollController?.position.maxScrollExtent ==
         widget.scrollController?.offset) {
       handleSelectedDateAndUserCallback(widget.initialDate!.add(Duration(days: 1)));
+      widget.scrollController?.animateTo(0, duration: Duration(milliseconds: 100), curve: Curves.ease);
     }
   }
 
