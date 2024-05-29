f572d7c7cbfa0ab93a2c1730dac9aae6ea3f9596
Mohsin Naqvi <Mohsinnaqvi606@gmail.com>
2024-05-29 09:12:02 +0500

    customization

--- lib/flutter_neat_and_clean_calendar.dart
+++ lib/flutter_neat_and_clean_calendar.dart
@@ -231,7 +231,7 @@ class _CalendarState extends State<Calendar> {
       Future.delayed(Duration(milliseconds: 10), () {
         if ((_selectedEvents?.isNotEmpty ?? false) &&
             (widget.scrollController?.hasClients ?? false)) {
-          widget.scrollController?.jumpTo(0);
+          // widget.scrollController?.jumpTo(0);
         }
       });
     } else if ((widget.scrollController?.hasClients ?? false) &&
@@ -241,7 +241,7 @@ class _CalendarState extends State<Calendar> {
       Future.delayed(Duration(milliseconds: 10), () {
         if ((_selectedEvents?.isNotEmpty ?? false) &&
             (widget.scrollController?.hasClients ?? false)) {
-          widget.scrollController?.jumpTo(0);
+          // widget.scrollController?.jumpTo(0);
         }
       });
     }
